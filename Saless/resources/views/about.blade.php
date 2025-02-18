a405727ec205089f2ec6ae5d13e2ae9e102f5a9d
Pawan3426 <pb2k77@gmail.com>
2025-02-18 10:06:23 +0545

    updated

--- Saless/resources/views/about.blade.php
+++ Saless/resources/views/about.blade.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <h1 class="bg-red-500 h-[12rem]">Laravel Project</h1>
+    <h1>About Page</h1>
     <a href="{{ route('home') }}">Home</a>
     <a href="{{ route('about') }}">About</a>
     <a href="{{ route('contact') }}">contact</a>
 </body>
-
 </html>
